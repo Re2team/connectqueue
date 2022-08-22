067cc066425e438ae531a9d9d863a64ede2876c9
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2022-08-22 13:59:08 +0200

    Fix the .0000000 after the seconds

--- server/sv_queue_config.lua
+++ server/sv_queue_config.lua
@@ -38,7 +38,7 @@ Config.GraceTime = 480
 
 Config.AntiSpam = false
 Config.AntiSpamTimer = 30
-Config.PleaseWait = "Please wait %f seconds. The connection will start automatically!"
+Config.PleaseWait = "Please wait %d seconds. The connection will start automatically!"
 
 -- on resource start, players can join the queue but will not let them join for __ milliseconds
 -- this will let the queue settle and lets other resources finish initializing
